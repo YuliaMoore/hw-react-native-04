e3b0583db4e23ab5f1e1d4b4547e4280dd279d78
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-10-10 19:29:48 +0300

    1

---
+++ src/Screens/Home/Home.js
@@ -0,0 +1,13 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Button,
+} from "recact-native";
+import React from "react";
+import { AntDesign, SimpleLineIcons, Feather } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navogation/bottom-tabs";
+import CreatePostsScreen from "../CreatePostsScreen/CreatePostsScreen";
+import PostsScreen from "../PostsScreen/PostsScreen";
+import ProfileScreen from "../ProfileScreen";
